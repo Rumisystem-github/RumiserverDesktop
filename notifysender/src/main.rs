a27f1abdffce6e08112105bcf990c4208e90b3d3
SINtyanneru <irjitgd@gmail.com>
2025-07-18 15:13:14 +0900

    通知発行

--- notifysender/src/main.rs
+++ notifysender/src/main.rs
@@ -1,3 +1,12 @@
+use notify_rust::Notification;
+
 fn main() {
-    println!("Hello, world!");
+    Notification::new()
+        .summary("新しい通知です")
+        .body("これはRustアプリケーションからのテスト通知です。")
+        .icon("dialog-information") // 通知に表示されるアイコン (OSによって表示方法が異なる場合があります)
+        .timeout(5000) // 通知の表示時間 (ミリ秒)
+		.appname("るみさーばーデスクトップ")
+        .show()
+        .unwrap();
 }
